24192b43271d6ec78d330783c97095e3dbc8c611
ecommerceevents <161142142+ecommerceevents@users.noreply.github.com>
2024-05-15 20:50:20 -0400

    Update shopifyplus.js

--- _data/sites/shopifyplus.js
+++ _data/sites/shopifyplus.js
@@ -1,1 +1,28 @@
-_data/sites/ssg.js
+module.exports = {
+	name: "SSG", // optional, falls back to object key
+	description: "Static Site Generator web sites",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 23, // 24 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "run"
+	},
+	urls: [
+		"https://eboost.com",
+    "https://monicarichkosann.com",
+    "https://soldejaneiro.com",
+    "https://forevernew.com",
+    "https://zitsticka.com",
+    "https://secondgearwnc.com",
+    "https://thinktankphoto.com",
+    "https://drakewaterfowl.com",
+    "https://boydsphila.com",
+    "https://obscuradigital.com",
+    "https://gvartwork.com"
+	]
+};
