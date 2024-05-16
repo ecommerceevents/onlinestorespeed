feb6b849425b0d2806d34eb9d3d59f4f115fd79d
ecommerceevents <161142142+ecommerceevents@users.noreply.github.com>
2024-05-15 20:51:02 -0400

    Update shopifyplus.js

--- _data/sites/shopifyplus.js
+++ _data/sites/shopifyplus.js
@@ -1,9 +1,6 @@
 module.exports = {
 	name: "SSG", // optional, falls back to object key
-	description: "Static Site Generator web sites",
-	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	description: "ShopifyPlus Lighthouse Metrics",
 	options: {
 		frequency: 60 * 23, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
